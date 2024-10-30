f528574d804d26c6625059e540bcbe2a7f6042ba
Rafi <rafi.ari.ghani@gmail.com>
2024-10-30 10:36:39 +0700

    make table for chart timestmap day and month

---
+++ app/Filament/Widgets/TableApp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Log;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class TableApp extends BaseWidget
+{
+    protected static ?string $heading = 'Table PerMonth';
+    protected static ?int $sort = 4;
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Log::query())
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                Tables\Columns\TextColumn::make('serial'),
+                Tables\Columns\TextColumn::make('volume'),
+                Tables\Columns\IconColumn::make('condition_io')
+                    ->label('condition')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Updated Volume')
+                    ->dateTime('Y-m-d'),
+            ]);
+    }
+}
